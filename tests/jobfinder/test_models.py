f523139b8ffb52301d4ae1dfdfe7f097077bc92a
maxo <max.orr@mail.com>
2025-07-09 09:26:12 -0400

    test: including summarization test

--- tests/jobfinder/test_models.py
+++ tests/jobfinder/test_models.py
@@ -1,10 +1,11 @@
 
 
-import pytest
 import pandas as pd
 from jobfinder import DATA_DIR
 from jobfinder.model import validate_defaults,found_jobs_from_df
 import logging
+
+from jobfinder.views.summarization_util import summarize_jobs
 
 logger = logging.getLogger(__name__)
 
@@ -22,3 +23,22 @@ def test_data_load():
         raise e
     logger.info("PASSED")
     
+    
+    
+def test_summarization_util():
+    
+
+    try:
+        _jobs_file = DATA_DIR.joinpath('jobs_data.csv')
+        df = pd.read_csv(_jobs_file)
+        validate_defaults(df)
+
+        # Test summarization with a small subset of jobs
+        sample_df = df.head(1)
+        summarize_jobs(sample_df)
+        assert sample_df['summary'].notnull().all(), "Summarization failed for some jobs."
+        print("Finished")
+
+    except Exception as e:
+        raise e
+    logger.info("PASSED")
