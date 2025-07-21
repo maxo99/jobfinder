b4accd2699356bd6fb63857228e3b14e0ab78c9b
maxo <max.orr@mail.com>
2025-07-21 14:12:03 -0400

    test: updating mockserver initializer for embed api

--- src/jobfinder/config.py
+++ src/jobfinder/config.py
@@ -1,7 +1,7 @@
 import os
 
 POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "password")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres")
 POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
 POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
 POSTGRES_DB = os.getenv("POSTGRES_DB", "jobfinder")
